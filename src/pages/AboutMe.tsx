380b660c0b31789b6daf94f73c50300624e017b0
SayefReyadh <sayef.reyadh.aust@gmail.com>
2024-04-22 20:36:49 +0600

    Update dependencies for react-router-dom and add new pages

---
+++ src/pages/AboutMe.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutMe() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>This is the about me page of our React application.</p>
+    </div>
+  );
+}
+
+export default AboutMe;
